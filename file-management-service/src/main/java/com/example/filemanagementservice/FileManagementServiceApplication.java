ef50d99451059e57bc692f828f7f91a835ecc2df
bushido <nakarasido5@gmail.com>
2024-02-26 19:44:36 +0700

    Initial commit

---
+++ file-management-service/src/main/java/com/example/filemanagementservice/FileManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.filemanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileManagementServiceApplication.class, args);
+	}
+
+}
---
+++ file-management-service/src/test/java/com/example/filemanagementservice/FileManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filemanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
